22d1557a025b7aad1b6428a242a0100e77ab7463
ROSTAND LELE <87706207+rosto-infinity@users.noreply.github.com>
2021-09-19 20:34:04 +0100

    etudiantCrud1

---
+++ resources/views/etudiant.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>etudiant</title>
+</head>
+<body>
+<h1>Liste des etudiants</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi reprehenderit blanditiis perferendis, perspiciatis harum vel quasi facere asperiores maxime! In beatae vitae officiis quod architecto et ducimus inventore dolorem provident!</p>
+</body>
+</html>
